3fbfdcf518b9578ca2e61b4a9b2028de87923e8e
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-06-25 17:25:44 -0300

    first commit

---
+++ packages/nodes-base/nodes/Sufficit/Sufficit.node.ts
@@ -0,0 +1,41 @@
+import {
+	IExecuteFunctions,
+} from 'n8n-core';
+
+import {
+	IDataObject,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
+
+import {
+	OptionsWithUri,
+} from 'request';
+
+export class FriendGrid implements INodeType {
+	description: INodeTypeDescription = {
+			displayName: 'Sufficit',
+			name: 'sufficit',
+			icon: 'file:sufficit.png',
+			group: ['transform'],
+			version: 1,
+			description: 'Consume Sufficit API',
+			defaults: {
+					name: 'Sufficit',
+					color: '#1A82e2',
+			},
+			inputs: ['main'],
+			outputs: ['main'],
+			credentials: [
+			],
+			properties: [
+					// Node properties which the user gets displayed and
+					// can change on the node.
+			],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+			return [[]];
+	}
+}
